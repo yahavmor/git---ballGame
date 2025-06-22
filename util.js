07b53dbc116a76f16597d98b3f13e907e2dc63c8
yahav mor <yahavmor77@gmail.com>
2025-06-22 12:10:09 +0300

    Refactor ball click handler to use random size increments and add utility functions for random color and integer generation

---
+++ util.js
@@ -0,0 +1,16 @@
+'use strict'
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; 
+}
+
+function getRandomColor() {
+  const letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
