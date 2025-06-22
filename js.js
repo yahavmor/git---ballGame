b031a082f7fb4846c4f6479b20c3d7cc1d170c23
yahav mor <yahavmor77@gmail.com>
2025-06-22 13:52:48 +0300

    Add click functionality to additional balls and update styles for better organization

--- js.js
+++ js.js
@@ -22,13 +22,35 @@ function onBallClick(elBall){
 }
 
 function changeOthersColors(thirdBall){
-    const elBalls = document.querySelectorAll('.ball');
+    const elBalls = document.querySelectorAll('.ball1, .ball2');
     for(var i = 0; i < elBalls.length; i++) {
-        if(elBalls[i] !== thirdBall){
-            onBallClick(elBalls[i])
+        onBallClick(elBalls[i])
+    }
+}
+function reduceDiameterOfOthers(forthBall){
+    const elBalls = document.querySelectorAll('.ball1, .ball2');
+    for(var i = 0; i < elBalls.length; i++) {
+            const currentWidth = parseInt(getComputedStyle(elBalls[i]).width);
+            const currentHeight = parseInt(getComputedStyle(elBalls[i]).height);
+
+            var newWidth = currentWidth - getRandomInt(20, 60);
+            var newHeight = currentHeight - getRandomInt(20, 60);
+
+            if(newWidth < 50 || newHeight < 50){
+                newWidth = 50;
+                newHeight = 50;
+            }
+
+            elBalls[i].style.width = newWidth + 'px';
+            elBalls[i].style.height = newHeight + 'px';
+            elBalls[i].querySelector('span').innerText = `${newWidth+newHeight}`;
         } 
 }
+
+function changePageColor(){
+    document.querySelector('body').style.backgroundColor = getRandomColor();
 }
+
 
 
 
