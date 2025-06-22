cffa4d61b4aa2b65f25cfaf698021fed849958b8
yahav mor <yahavmor77@gmail.com>
2025-06-22 11:14:15 +0300

    Update ball click handler to resize and update text

--- js.js
+++ js.js
@@ -1,5 +1,23 @@
 'use strict'
 
-function onBallClick(){
-    console.log('Ball clicked!');
+function onBallClick(elBall){
+    const currentWidth = parseInt(getComputedStyle(elBall).width);
+    const currentHeight = parseInt(getComputedStyle(elBall).height);
+
+
+    var newWidth = currentWidth + 50;
+    var newHeight = currentHeight + 50;
+
+    if(newWidth+newHeight > 400){
+        newWidth = 50
+        newHeight = 50
+    }
+
+
+    elBall.style.width = newWidth + 'px';
+    elBall.style.height = newHeight + 'px';
+
+    elBall.querySelector('span').innerText = `${newWidth+newHeight}`;
+
 }
+
